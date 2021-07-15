c1a99297f03cbf58b3d4df9eab09e9bf84351c89
luca <lucaimbalzano@gmail.com>
2021-07-15 08:50:22 +0200

    ##Routing --forChild Dashboard styleCss r03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  {path: 'Heroes', component: HeroesComponent}
+  {path: 'Heroes', component: HeroesComponent},
+  {path: 'Dashboard', component: DashboardComponent},
+  {path: '', redirectTo: 'Dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
